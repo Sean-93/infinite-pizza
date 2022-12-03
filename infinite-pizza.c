4266c493faa3b6a0de3ef3e403bc57c5401072d4
unknown <seandanielrooney@gmail.com>
2022-12-02 19:59:52 -0800

    got delay and speed increment worked out

--- infinite-pizza.c
+++ infinite-pizza.c
@@ -1,7 +1,98 @@
 #include <stdio.h>
+#include <windows.h>
 
-int main()
+void infinitePizzaIntro()
 {
-    printf("\n               PIZZA!!!\n __________________________________\n(____(____(____(____)____)____)____)\n \\********************************/\n  \\********    ******************/\n   \\*****        ***************/\n    \\****        *****    *****/\n     \\*****    *****        **/\n      \\*************        */\n       \\**************    **/\n        \\******************/\n         \\****    ********/\n          \\*        *****/\n           \\        ****/\n            \\*    *****/\n             \\********/\n              \\******/\n               \\****/\n                \\**/\n                 \\/\n\n               PIZZA!!!\n");
-    return 0;
+  printf("\n\tINFINITE PIZZA INITIALIZING...");
+  Sleep(1000);
+  printf("\n\n\t\t\t3");
+  Sleep(1000);
+  printf("\n\n\t\t\t2");
+  Sleep(1000);
+  printf("\n\n\t\t\t1\n");
+  Sleep(1000);
+}
+
+// 2 DIFFERENT FUNCTIONS FOR PRINTING ASCII ART OF PIZZA- WITH MIRRORED PEPPERONI PATTERN (I CHOSE 2 SO THAT THE SPEED INCREMENT INCREASE IS MORE NOTICEABLE)
+
+void printPizzaOne()
+{
+  printf("\n\t __________________________________\n\t(____(____(____(____)____)____)____)\n\t \\********************************/\n\t  \\********    ******************/\n\t   \\*****        ***************/\n\t    \\****        *****    *****/\n\t     \\*****    *****        **/\n\t      \\*************        */\n\t       \\**************    **/\n\t        \\******************/\n\t         \\****    ********/\n\t          \\*        *****/\n\t           \\        ****/\n\t            \\*    *****/\n\t             \\********/\n\t              \\******/\n\t               \\****/\n\t                \\**/\n\t                 \\/\n\n\n\n\n");
+}
+
+void printPizzaTwo()
+{
+  printf("\n\t __________________________________\n\t(____(____(____(____)____)____)____)\n\t \\********************************/\n\t  \\*******************    *******/\n\t   \\****************        ****/\n\t    \\******    *****        ***/\n\t     \\***        *****    ****/\n\t      \\**        ************/\n\t       \\***    *************/\n\t        \\******************/\n\t         \\********    ****/\n\t          \\*****        */\n\t           \\****        /\n\t            \\*****    */\n\t             \\********/\n\t              \\******/\n\t               \\****/\n\t                \\**/\n\t                 \\/\n\n\n\n\n");
+}
+
+void infinitePrintPizzaLoop()
+{
+
+  int pizzaPrintSleepMilliseconds = 3000;
+
+  int i = 1;
+
+  // WHILE LOOP FOR PRINTING WHEN DELAY IS MORE THAN 1000 MILLISECONDS
+
+  while (i = 1 && pizzaPrintSleepMilliseconds > 1000)
+  {
+    printPizzaOne();
+    Sleep(pizzaPrintSleepMilliseconds);
+    printPizzaTwo();
+    Sleep(pizzaPrintSleepMilliseconds);
+
+    pizzaPrintSleepMilliseconds -= 500;
+
+    // printf("%d", pizzaPrintSleepMilliseconds);
+    printf("\n\tThis is a lot of pizza.");
+  }
+
+  // WHILE LOOP FOR PRINTING WHEN DELAY IS MORE THAN 500 MILLISECONDS
+
+  while (i = 1 && pizzaPrintSleepMilliseconds > 100)
+  {
+    printPizzaOne();
+    Sleep(pizzaPrintSleepMilliseconds);
+    printPizzaTwo();
+    Sleep(pizzaPrintSleepMilliseconds);
+
+    pizzaPrintSleepMilliseconds -= 50;
+
+    // printf("%d", pizzaPrintSleepMilliseconds);
+    printf("\n\tOh no....");
+  }
+
+  // WHILE LOOP FOR PRINTING WHEN DELAY IS MORE THAN 500 MILLISECONDS
+
+  while (i = 1 && pizzaPrintSleepMilliseconds > 50)
+  {
+    printPizzaOne();
+    Sleep(pizzaPrintSleepMilliseconds);
+    printPizzaTwo();
+    Sleep(pizzaPrintSleepMilliseconds);
+
+    pizzaPrintSleepMilliseconds--;
+
+    // printf("%d", pizzaPrintSleepMilliseconds);
+    printf("\n\tThere are WAY too many pizzas!");
+  }
+
+  // INFINITE WHILE LOOP FOR PRINTING WHEN DELAY IS EQUAL TO 1 MILLISECOND
+
+  while (pizzaPrintSleepMilliseconds = 1)
+  {
+    printPizzaOne();
+    Sleep(pizzaPrintSleepMilliseconds);
+    printPizzaTwo();
+    Sleep(pizzaPrintSleepMilliseconds);
+
+    // printf("%d", pizzaPrintSleepMilliseconds);
+    printf("\n\tINFINITE PIZZAAAAAA!!!");
+  }
+}
+
+void main()
+{
+  infinitePizzaIntro();
+  infinitePrintPizzaLoop();
 }
